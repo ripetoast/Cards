0a5468d95ec5b2eb1fd40454b6987cd19df50fd0
Michael <michael_lau@live.co.uk>
2020-05-06 02:04:15 +0100

    ADD: Input card component data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpeg",
+      username: "nature",
+      content: "I saw this neat tree today"  
+    },
+    {
+      title: "Snowy Mountain",
+      imageUrl: "assets/mountain.jpeg",
+      username: "mountain",
+      content: "I saw this neat mountain today"
+    },
+    {
+      title: "Mountain Biking",
+      imageUrl: "assets/biking.jpeg",
+      username: "biking",
+      content: "I saw this neat mountain bike today"
+    }
+  ]
 }
